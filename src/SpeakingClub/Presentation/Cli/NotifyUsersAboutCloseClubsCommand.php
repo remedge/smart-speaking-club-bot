21aca7dc95704119b2e16abad4ffc9cae93634b3
Aleksandr Dorožkin <merk.spb@gmail.com>
2023-05-31 23:07:28 +0200

    Update club notification text

--- src/SpeakingClub/Presentation/Cli/NotifyUsersAboutCloseClubsCommand.php
+++ src/SpeakingClub/Presentation/Cli/NotifyUsersAboutCloseClubsCommand.php
@@ -39,7 +39,7 @@ class NotifyUsersAboutCloseClubsCommand extends Command
             $participations = $this->participationRepository->findBySpeakingClubId($speakingClub->getId());
 
             foreach ($participations as $participation) {
-                $this->telegram->sendMessage((int) $participation['chatId'], sprintf('Разговорный клуб "%s" начнется через 24 часа', $speakingClub->getName()));
+                $this->telegram->sendMessage((int) $participation['chatId'], sprintf('Разговорный клуб "%s" начнется через 24 часа. Если у вас не получается прийти, пожалуйста, отмените вашу запись, чтобы мы предложили ваше место другим.', $speakingClub->getName()));
             }
         }
 
@@ -55,7 +55,7 @@ class NotifyUsersAboutCloseClubsCommand extends Command
             $participations = $this->participationRepository->findBySpeakingClubId($speakingClub->getId());
 
             foreach ($participations as $participation) {
-                $this->telegram->sendMessage((int) $participation['chatId'], sprintf('Разговорный клуб "%s" начнется через 2 часа', $speakingClub->getName()));
+                $this->telegram->sendMessage((int) $participation['chatId'], sprintf('Разговорный клуб "%s" начнется через 2 часа. Если у вас не получается прийти, пожалуйста, отмените вашу запись, чтобы мы предложили ваше место другим.', $speakingClub->getName()));
             }
         }
 
